de6af0c02668df24b537e94ff9961088f57b6926
Md-Rijwan-Jannat <rijwanjannat36@gmail.com>
2023-05-03 14:12:38 +0600

    chef data load

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 5000 ;
+const port = process.env.PORT || 5000;
+const chef = require('./Data/chef.json')
 
 app.get('/', (req, res) => {
   res.send('the-chef-recipe-server running')
+})
+app.get('/chef', (req, res) => {
+  res.send(chef);
 })
 
 app.listen(port, () => {
