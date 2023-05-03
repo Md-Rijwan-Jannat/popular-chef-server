4c08d839807b37f81b16d2c1de15869116b91b5e
Md-Rijwan-Jannat <rijwanjannat36@gmail.com>
2023-05-04 00:26:57 +0600

    modefied

--- index.js
+++ index.js
@@ -16,11 +16,17 @@ app.get('/chef', (req, res) => {
 app.get('/recipe', (req, res) => {
   res.send(recipe)
 })
-app.get('/recipe/:id', (req, res) => {
-  const id = parseInt(req.params.id);
-  const selectedId = recipe.find(n => parseInt(n._id) === id)
-  res.send(selectedId)
+app.get('/chef/:id', (req, res) => {
+  const id = parseInt(req.params.id)
+  if (id === 0) {
+      res.send(recipe)
+  } else {
+      const categoryNews = recipe.filter(n => parseInt(n._id) === id)
+      res.send(categoryNews);
+  }
 })
+
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
